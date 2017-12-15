9617f3ce9fe1f02a4e2f7af67b90a7c645c7a668
Juan Manuel Mougan <juanmougan@gmail.com>
2017-12-15 17:59:20 -0300

    Started penalty jobs

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,12 @@
 class Match < ApplicationRecord
   belongs_to :home_team, :class_name => 'Team', :foreign_key => 'home_team'
   belongs_to :away_team, :class_name => 'Team', :foreign_key => 'away_team'
+
+  after_save :process_incidences, on: :create
+
+  protected
+  def process_incidences
+    ProcessBookedPlayers.perform_later guest
+  end
+
 end
