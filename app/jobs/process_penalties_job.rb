0aa36794f34f5d6bc37a8422e04b64eb5dda972c
Juan Manuel Mougan <juanmougan@gmail.com>
2017-12-20 23:38:50 -0300

    The job creates the penalty

--- app/jobs/process_penalties_job.rb
+++ app/jobs/process_penalties_job.rb
@@ -12,9 +12,15 @@ class ProcessPenaltiesJob < ApplicationJob
     yellows.each {|player, cards| do
       if cards > 1
         # Create Penalty model
-        # penalty = Penalty.create :player_id => p.id, :status => "active"
+        incidences = Incidence.incidences_for_player_id_and_incidence_type(player.id, 1).order(:match_id)
+        # I need the Incidence of the last match to create the Penalty
+        penalty = Penalty.create(
+          player_id: player.id,
+          match_id: incidences.last.match_id,
+          length: 1,    # Yellow card has one match only
+          status: 1
+        )
         # Deactivate Incidences for this Player
-        incidences = Incidence.incidences_for_player_id_and_incidence_type(player.id, 1)
         incidences.each do |i|
           i.status = 2    # 2 is inactive
           i.save!
