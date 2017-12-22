6f2f4ed4d2f12c2c3191a306e0df030529088d9f
Juan Manuel Mougan <juanmougan@gmail.com>
2017-12-22 17:36:05 -0300

    Fix compilation errors and Hash serialization

--- app/jobs/process_penalties_job.rb
+++ app/jobs/process_penalties_job.rb
@@ -9,13 +9,13 @@ class ProcessPenaltiesJob < ApplicationJob
   end
 
   def create_penalties_for_yellow_cards(yellows)
-    yellows.each {|player, cards| do
+    yellows.each do |player_id, cards|
       if cards > 1
         # Create Penalty model
-        incidences = Incidence.incidences_for_player_id_and_incidence_type(player.id, 1).order(:match_id)
+        incidences = Incidence.incidences_for_player_id_and_incidence_type(player_id, 1).order(:match_id)
         # I need the Incidence of the last match to create the Penalty
         penalty = Penalty.create(
-          player_id: player.id,
+          player_id: player_id,
           match_id: incidences.last.match_id,
           length: 1,    # Yellow card has one match only
           status: 1
@@ -32,14 +32,14 @@ class ProcessPenaltiesJob < ApplicationJob
   # TODO remove duplication here
   # Pass a Block with common behaviour? Pass the condition as parameter?
   def create_penalties_for_red_cards(reds)
-    reds.each {|player, cards| do
+    reds.each do |player_id, cards|
       if cards > 0
         # Create Penalty model
         # penalty = Penalty.create :player_id => p.id, :status => "active"
         # Deactivate Incidences for this Player
-        incidences = Incidence.incidences_for_player_id_and_incidence_type(player.id, 2).order(:match_id)
+        incidences = Incidence.incidences_for_player_id_and_incidence_type(player_id, 2).order(:match_id)
         penalty = Penalty.create(
-          player_id: player.id,
+          player_id: player_id,
           match_id: incidences.last.match_id,
           length: 2,    # Red card has two matches (for now)
           status: 1
--- app/models/incidence.rb
+++ app/models/incidence.rb
@@ -14,8 +14,8 @@ class Incidence < ApplicationRecord
     yellows = calculate_cards(1)
     reds = calculate_cards(2)
     cards << yellows << reds
-    ProcessPenalties.perform_later cards
-    PenaltiesFinalizer.perform_later self.match
+    ProcessPenaltiesJob.perform_later cards
+    PenaltiesFinalizerJob.perform_later self.match
   end
 
   def calculate_cards(card_type)
@@ -24,7 +24,7 @@ class Incidence < ApplicationRecord
     players.each { |p|
       # 1 is yellow, 2 is red
       counted_cards = Incidence.incidences_for_player_id_and_incidence_type(p.id, card_type).size
-      cards[p] = counted_cards
+      cards[p.id] = counted_cards
     }
     return cards
   end
