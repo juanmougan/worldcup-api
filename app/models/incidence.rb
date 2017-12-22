18e0114dcb809c4c5941bc2171eb5c7c4778ecb1
Juan Manuel Mougan <juanmougan@gmail.com>
2017-12-22 12:45:46 -0300

    Added first job logic into Incidences... because I don't know how to run jobs in a series (yet)

--- app/models/incidence.rb
+++ app/models/incidence.rb
@@ -10,6 +10,22 @@ class Incidence < ApplicationRecord
 
   protected
   def process_incidences
-    ProcessBookedPlayers.perform_later guest
+    cards = []
+    yellows = calculate_cards(1)
+    reds = calculate_cards(2)
+    cards << yellows << reds
+    ProcessPenaltiesJob.perform_later cards
+    PenaltiesFinalizerJob.perform_later self.match
+  end
+
+  def calculate_cards(card_type)
+    cards = Hash.new
+    players = Player.fetch_players_with_active_incidences
+    players.each { |p|
+      # 1 is yellow, 2 is red
+      counted_cards = Incidence.incidences_for_player_id_and_incidence_type(p.id, card_type).size
+      cards[p] = counted_cards
+    }
+    return cards
   end
 end
